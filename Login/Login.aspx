37fb73ef585372767414ca9307414e4574b63fb1
venkatesh2208 <venki.venkatesh496@gmail.com>
2019-05-01 17:36:20 -0400

    New code added

---
+++ Login/Login.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Login.aspx.cs" Inherits="Login_Login" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Untitled Page</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+    </div>
+    <asp:TextBox ID="txt_name" style="position:absolute; top: 182px; left: 446px;" 
+        runat="server"></asp:TextBox>
+    <asp:TextBox ID="txt_psw" style="position:absolute; top: 254px; left: 445px;" 
+        runat="server" TextMode="Password"></asp:TextBox>
+    <asp:Label ID="Label1" style="position:absolute; top: 188px; left: 229px; height: 19px;" 
+        runat="server" Text="Employee Id:" ForeColor="#0033CC"></asp:Label>
+    <asp:Label ID="Label2" 
+        style="position:absolute; top: 261px; left: 228px; height: 30px; width: 120px;" 
+        runat="server" Text="Emp Password:" ForeColor="#0033CC"></asp:Label>
+    <asp:Button ID="Button2" style="position:absolute; top: 346px; left: 353px; width: 67px;" 
+        runat="server" Text="Submit" ForeColor="#0033CC" onclick="Button2_Click" />
+    <asp:Label ID="Label3"  
+        style="position:absolute; top: 51px; left: 226px; width: 460px; height: 36px;" 
+        runat="server" Text="Welcome to Login page" Font-Bold="True" Font-Italic="True" 
+        Font-Names="Magneto" Font-Size="XX-Large" ForeColor="#0033CC"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ Login/Login.aspx.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using System.Data.SqlClient;
+
+public partial class Login_Login : System.Web.UI.Page
+{
+    SqlConnection con = new SqlConnection("Data Source=AKITS-CSE\\SQLEXPRESS;Initial Catalog=TimeManagement;Integrated Security=True");
+    SqlCommand cmd;
+    SqlDataReader dr;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void Button2_Click(object sender, EventArgs e)
+    
+    {
+        /*
+        string fname = txt_name.Text;
+        string psw = txt_psw.Text;
+
+        string sql = "select * from empinsert where firstname='" + fname + "' and paswd='" + psw + "'";
+
+        DataSet ds = new DataSet();
+        ds = DAL.SqlHelper.ExecuteDataset(clsConnection.Connection, CommandType.Text, sql);
+
+        if (ds.Tables[0].Rows.Count == 1)
+        {
+            if (ds.Tables[0].Rows[0]["position"].ToString() = "Team Member")
+            {
+                Response.Redirect("~/Emp/Emphome.aspx");
+
+            }
+            else if (ds.Tables[0].Rows[0]["position"].ToString() = "MANAGER")
+            {
+                Response.Redirect("~/Manager/Man Home.aspx");
+            }
+            
+
+        }
+        else
+        {
+            Response.Write("invalisd");
+ 
+        }
+         
+        */
+
+
+
+
+        if (txt_name.Text == "admin" && txt_psw.Text == "admin")
+        {
+            Response.Redirect("Home.aspx");
+        }
+        con.Open();
+        string emp_id = txt_name.Text;
+        string psw = txt_psw.Text;
+        string sql = "select * from empinsert where empid='" + emp_id + "' and paswd='" + psw + "'";
+        SqlDataAdapter da = new SqlDataAdapter(sql, con);
+        DataTable dt = new DataTable();
+        da.Fill(dt);
+        if (dt.Rows.Count == 1)
+        {
+           Session["firstname"] = emp_id;
+            if (dt.Rows[0]["position"].ToString() == "MANAGER")
+            {
+                Response.Redirect("~/Manager/Man Home.aspx");
+            }
+            else if (dt.Rows[0]["position"].ToString() == "EMPLOYEE")
+            {
+                Response.Redirect("~/Emp/Home.aspx");
+            }
+            else if (dt.Rows[0]["position"].ToString() == "ADMIN")
+            {
+                Response.Redirect("~/Admine/Home.aspx");
+            }
+            else
+            {
+                Response.Write("invalisd");
+            }
+            }
+        }
+        
+
+
+        
+
+    }
+   
